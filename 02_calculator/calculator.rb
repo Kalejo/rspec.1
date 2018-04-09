7fa5a8940fb7ee159794991094c1302d7f2fc663
Kalejo <alejo_alvarez67@hotmail.com>
2018-04-09 19:01:10 +0200

    first commit

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,54 @@
+#write your code here
+def add (premier_parametre, deuxieme_parametre)
+	premier_parametre + deuxieme_parametre
+end
+
+def subtract (premier_parametre, deuxieme_parametre)
+	premier_parametre - deuxieme_parametre
+end
+
+
+def sum(chiffres)
+  total = 0
+  chiffres.each { |chiffre| total += chiffre }
+  total
+end
+
+
+def multiply(premier_parametre, *autres_chiffres)
+  total = premier_parametre
+  others.each { |chiffre| total *= chiffre }
+  total
+end
+
+
+def multiply(premier_parametre, *autres_chiffres)
+  total = premier_parametre
+ end
+
+
+
+
+=begin
+	
+rescue Exception => e
+	
+end
+def sum(*x)
+    total = 0    
+    if [].empty? 
+        return sum = 0
+     else 
+         x.each { |nombre| 
+         total += nombre
+         puts total
+         }    
+
+     end
+end 
+=begin
+	
+*Créer un hash pour stocker les nombres 
+*Obtenir un tableau des nombres de 	
+end
+=end
---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,7 @@
+#write your code here
+def echo(string)
+	string
+end
+def shout (string)
+	string.upcase
+end
